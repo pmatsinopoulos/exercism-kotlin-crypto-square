b2d93b56b8d782123795aa46a68ede8df2743552
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-31 13:12:07 +0300

    Use String.chunked to break the long string in chunks

--- src/main/kotlin/CryptoSquare.kt
+++ src/main/kotlin/CryptoSquare.kt
@@ -3,7 +3,7 @@ private fun normalize(originalText: String): String {
     return originalText.replace(regex, "").lowercase()
 }
 
-private fun calculateColumnsAndRows(length: Int): Array<Int> {
+private fun calculateColumnsAndRows(length: Int): List<Int> {
     var c = 1
     var r = length / c
     while (c - r > 1 || c < r) {
@@ -11,31 +11,18 @@ private fun calculateColumnsAndRows(length: Int): Array<Int> {
         r = length / c
         if (length % c >= 1) r++
     }
-    return arrayOf(c, r)
+    return listOf(c, r)
 }
 
-private fun breakTextByColumns(text: String, columns: Int): Array<String> {
-    var element = ""
-    var result = arrayOf<String>()
-    text.forEach { char ->
-        if (element.length < columns) {
-            element = element + char.toString()
-        } else {
-            result = result.plus(element)
-            element = char.toString()
-        }
-    }
-    if (element.isNotBlank()) {
-        result = result.plus(element.padEnd(columns, ' '))
-    }
-    return result
+private fun breakTextByColumns(text: String, columns: Int): List<String> {
+    return text.chunked(columns) { c -> c.padEnd(columns).toString() }
 }
 
-private fun buildEncodedText(input: Array<String>): String {
+private fun buildEncodedText(input: List<String>): String {
     var result = ""
     for (i in 0 until input[0].length) {
-        for (j in 0 until input.size) {
-            result = "${result}${input[j][i]}"
+        for (inputPhrase in input) {
+            result = "${result}${inputPhrase[i]}"
         }
         if (i < input[0].length - 1) result = "$result "
     }
@@ -52,7 +39,7 @@ object CryptoSquare {
 
         val columnsAndRows = calculateColumnsAndRows(length)
 
-        val textByColumns: Array<String> = breakTextByColumns(result, columnsAndRows[0])
+        val textByColumns: List<String> = breakTextByColumns(result, columnsAndRows[0])
         result = buildEncodedText(textByColumns)
 
         return result
